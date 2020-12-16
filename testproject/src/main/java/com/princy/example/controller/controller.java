ac00d7e07e8a1b97f85c4427ec439909e7b9d216
praty <praty@DESKTOP-PDMBUS9>
2020-12-16 13:52:41 -0500

    forcefully pushed error

--- testproject/src/main/java/com/princy/example/controller/controller.java
+++ testproject/src/main/java/com/princy/example/controller/controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class controller {
 	@PostMapping("/")
-	public String gitHub(){
+	public gitHub(){
 		return "Git hub is successfully running";
 	}
 	
